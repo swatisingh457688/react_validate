6922d82796a2c71785c6e05e8972a5433beace67
swatisingh457688 <114169440+swatisingh457688@users.noreply.github.com>
2024-06-17 08:55:17 +0530

    swati

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,6 +5,11 @@ import { db } from './firestore'
 import { addDoc, collection, getDocs } from 'firebase/firestore'
 
 
+
+
+
+
+
 let obj = {}
 export default function App() {
 
@@ -12,90 +17,34 @@ export default function App() {
   return (
     <>
 
-      <form action="">
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        let form = new FormData(e.target)
+        let obj = Object.fromEntries(form)
+        console.log(obj)
+      }} action="">
         {Register ? <h1>sign in</h1> : <h1>sign up</h1>}
         {Register ? '' : <div>
-          <input
-            onChange={(e) => {
-              // console.log(e.target.value)
-              obj.name = e.target.value
-            }}
-            name='name' type="text" placeholder='Username' />
+          <input name='name' type="text" placeholder='Username' />
         </div>}
 
         <div>
-          <input
-            onChange={(e) => {
-              // console.log(e.target.value)
-              obj.email = e.target.value
-            }}
-            name='email' type="text" placeholder='Email' />
+          <input name='email' type="text" placeholder='Email' />
         </div>
 
         <div>
-          <input
-            onChange={(e) => {
-              // console.log(e.target.value)
-              obj.pass = e.target.value
-            }}
-            name='pass' type="text" placeholder='Password' />
+          <input name='pass' type="text" placeholder='Password' />
         </div>
 
         {Register ? '' : <div>
-          <input
-            onChange={(e) => {
-              // console.log(e.target.value)
-              obj.cpass = e.target.value
-            }}
-            name='cpass' type="text" placeholder='ConfirmPass' />
+          <input name='cpass' type="text" placeholder='ConfirmPass' />
         </div>}
 
         <div>
 
           {Register ? '' : <button
 
-            className='cc form-control' onClick={async (e) => {
-              e.preventDefault()
-
-
-              if ((obj.name !== '') && (obj.email !== '') && (obj.pass !== '') && (obj.cpass !== '')) {
-
-
-                if (obj.pass === obj.cpass) {
-
-                  let valid = true
-
-                  const querySnapshot = await getDocs(collection(db, "users"));
-
-                  for (let i = 0; i < querySnapshot.docs.length; i++) {
-                    if (querySnapshot.docs[i].data().email === obj.email) {
-                      valid = false
-                      break
-                    }
-
-                  }
-
-                  if (valid) {
-                    try {
-                      const docRef = await addDoc(collection(db, "users"), obj);
-                      console.log(docRef.id)
-                      alert('successfully registred')
-                    }
-                    catch (e) {
-                      console.error('error added')
-
-                    }
-                  } else {
-                    alert('email already registred')
-                  }
-                } else {
-                  alert('incorrect password')
-                }
-              } else {
-                alert('empty field')
-              }
-
-            }}>
+            className='cc form-control signup' >
             Create
           </button>}
 
@@ -104,32 +53,7 @@ export default function App() {
 
 
           {Register ?
-            <button
-              onSubmit={async (e) => {
-                e.preventDefault()
-                let form = new FormData(e.target)
-                let obj = Object.fromEntries(form)
-
-
-                const querySnapshot = await getDocs(collection(db, "users"));
-                console.log(querySnapshot.docs[0].data().email)
-                let valid = true
-                for (let i = 0; i < querySnapshot.docs.length; i++) {
-                  if (querySnapshot.docs[i].data().email === obj.email) {
-                    valid = false
-                    localStorage.setItem('status', JSON.stringify(querySnapshot.docs[i].data()))
-                    alert('123')
-                    window.location.href = './'
-                    break
-                  }
-
-                }
-                if (valid) {
-                  alert('Email Not Registered')
-                }
-              }}
-
-              className='cc form-control' >
+            <button className='cc form-control login' >
               Login
             </button> : ''}
           {Register ?
@@ -144,3 +68,5 @@ export default function App() {
     </>
   )
 }
+
+
