b6b432ff53b5a08f7c9bc214ff201aee029d1674
swatisingh457688 <114169440+swatisingh457688@users.noreply.github.com>
2024-06-22 22:51:02 +0530

    swati

--- my-react-app/src/firestore.js
+++ my-react-app/src/firestore.js
@@ -1,8 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,6 +15,51 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+export const storage = getStorage(app);
 
-export { db }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from 'firebase/firestore';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAC9C0SyZNpIojxCDgGD19eBJ0fjGhwIWw",
+//     authDomain: "formtemp-55058.firebaseapp.com",
+//     projectId: "formtemp-55058",
+//     storageBucket: "formtemp-55058.appspot.com",
+//     messagingSenderId: "732862648085",
+//     appId: "1:732862648085:web:18dd60ca7a8326a801231c"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app)
+
+// export { db }
+
+
+
